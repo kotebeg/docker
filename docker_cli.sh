64dedfb8cb62f9bd5f7d9a53c3e3883353fbf060
kotebeg <49250158+kotebeg@users.noreply.github.com>
2025-05-27 19:58:39 +0400

    Update docker_cli.sh
    removing containers

--- docker_cli.sh
+++ docker_cli.sh
@@ -3,3 +3,10 @@ docker run -p 5005:5005 your-image-name
 
 # 🧠 Tip: If You Want to Run in Background (Detached Mode)
 docker run -d -p 5005:5005 your-image-name
+
+# 🔥 1. Stop the Container (if running)
+docker stop <container_id_or_name>
+docker stop my_container
+
+# 🧹 2. Remove the Container
+docker rm <container_id_or_name>
